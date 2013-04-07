bb72981bb33a24cb4cb78bdb1889533b165b622e
yhaz <zhouandy@gmail.com>
2013-04-06 22:37:00 -0700

    add exercise 1-1 and 1-2

---
+++ chapter-1/1-1.c
@@ -0,0 +1,7 @@
+/* K&R page 8  Exercise 1-1 */
+
+#include <stdio.h>
+
+main() {
+	printf("hello, world\n");
+}
---
+++ chapter-1/1-2.c
@@ -0,0 +1,8 @@
+/* K&R page 8  Exercise 1-2 */
+
+#include <stdio.h>
+
+main() {
+	printf("hello, world\n");
+	printf("%s\n", "\\a");
+}
