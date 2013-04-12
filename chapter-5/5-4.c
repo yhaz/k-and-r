793740a4ba136724b6ca4ee51f9d3d6d24bd20f8
yhaz <zhouandy@gmail.com>
2013-04-11 19:17:06 -0700

    add the 2nd solutionto 5-4.c

--- chapter-5/5-4.c
+++ chapter-5/5-4.c
@@ -7,9 +7,10 @@
 #include <string.h>
 
 /* Returns 1 if string 't' occurs at the end of string 's',
-   and zero otherwise  
+   and zero otherwise. This version uses c library functions
+   to make it easier to implment.
 */
-int strend(char *s, char *t)
+int strend_simple(char *s, char *t)
 {
 	int sl = strlen(s), tl = strlen(t);
 
@@ -17,6 +18,30 @@ int strend(char *s, char *t)
 		return 0;
 
 	return (strcmp(s + sl - tl, t) == 0);
+}
+
+/* Returns 1 if string 't' occurs at the end of string 's',
+   and zero otherwise.
+*/
+int strend(char *s, char *t)
+{
+	char *se, *te;
+
+	/* Get to the end of each string */
+	for (se = s; *se != '\0'; se++)
+	;
+	for (te = t; *te != '\0'; te++)
+	;
+
+	if ( (te - t)  > (se - s) )
+		return 0;
+
+	/* Compare all 'te' and 'se' in reverse order, except the
+           last charater. */
+	for (; (*te == *se) && (te > t); se--, te--)
+	;
+
+	return (*te == *se);
 }
 
 main()
